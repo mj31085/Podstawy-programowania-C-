dec446a623a1b212dfc8e845ed9b4c23d1bc2c8a
Maciej Jakubowski <mj31085@zpsb.pl>
2026-03-08 10:55:17 +0100

    Dodanie zadania Zmienna

---
+++ Zadania - pierwszy program w C++/Zmienna.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+int main() {
+	int a = 5;
+	int b = 3;
+    
+    std::cout << "Liczba a: " << a << std::endl;
+    std::cout << "Liczba b: " << b << std::endl;
+    
+    std::cout << "Suma a + b: " << (a + b) << std::endl;
+    std::cout << "Roznica a - b: " << (a - b) << std::endl;
+    std::cout << "Iloraz a * b: " << (a * b) << std::endl;
+    
+    return 0;
+}
