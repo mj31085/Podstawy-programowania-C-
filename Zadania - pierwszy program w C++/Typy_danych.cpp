3cd21bd283cdf9107c005e837249577da4032cb6
Maciej Jakubowski <mj31085@zpsb.pl>
2026-03-29 09:24:46 +0200

    Added Typy_danych.cpp.

---
+++ Zadania - pierwszy program w C++/Typy_danych.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+int main() {
+	int a = 10;
+	float b = 3.14;
+	char c = 'A';
+	bool  d = true;
+	
+	std::cout << "Zmienna całkowita: " << a << std::endl;
+	std::cout << "Zmienna zmiennoprzecinkowa: " << b << std::endl;
+	std::cout << "Zmienna znakowa: " << c << std::endl;
+	std::cout << "Zmienna logiczna: " << d << std::endl;
+}
